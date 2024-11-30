6f4282f18c1b4bad51c8c7b0e19a35405cfed433
prhehns <pmalatuba@gmail.com>
2024-11-30 22:31:22 +0800

    logic for user order history

--- cc206_WVSU Buy and Sell/lib/features/screens/cart/cart_model.dart
+++ cc206_WVSU Buy and Sell/lib/features/screens/cart/cart_model.dart
@@ -5,18 +5,16 @@ class CartItem {
   final String title;
   final double price;
   final String imageUrl;
-  final String sellerId;
-  final String sellerName;
-  late int quantity; // Fixed to use an integer quantity
+  final String sellerId;  // Seller ID
+  late int quantity;      // Fixed to use an integer quantity
 
   CartItem({
     required this.id,
     required this.title,
     required this.price,
     required this.imageUrl,
-    required this.sellerId, // Seller ID
-    required this.sellerName,
-    this.quantity = 1, // Default quantity set to 1 if not passed
+    required this.sellerId,  // Seller ID
+    this.quantity = 1,       // Default quantity set to 1 if not passed
   });
 }
 
@@ -26,8 +24,7 @@ class CartModel with ChangeNotifier {
   List<CartItem> get items => _items;
 
   // Add an item to the cart
-  void addToCart(String productId, String title, double price, String imageUrl,
-      String sellerId, String sellerName) {
+  void addToCart(String productId, String title, double price, String imageUrl, String sellerId) {
     // Check if the item is already in the cart
     final existingItemIndex = _items.indexWhere((item) => item.id == productId);
 
@@ -37,12 +34,11 @@ class CartModel with ChangeNotifier {
     } else {
       // Otherwise, add the new item with default quantity of 1
       final newItem = CartItem(
-        id: productId, // The productId (post_id)
+        id: productId,  // The productId (post_id)
         title: title,
         price: price,
         imageUrl: imageUrl,
-        sellerId: sellerId, // The seller ID
-        sellerName: sellerName, // The seller name
+        sellerId: sellerId,  // The seller ID
       );
       _items.add(newItem);
     }
@@ -68,10 +64,7 @@ class CartModel with ChangeNotifier {
 
   // Calculate total price
   double get totalPrice {
-    return _items.fold(
-        0.0,
-        (total, currentItem) =>
-            total + currentItem.price * currentItem.quantity);
+    return _items.fold(0.0, (total, currentItem) => total + currentItem.price * currentItem.quantity);
   }
 
   // Clear the cart
